cf7feb10bea58ad635e098388efc51f7626d0f57
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-08-15 19:34:29 +0300

    update providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { SidebarProvider } from '@/context/sidebar';
 import { getQueryClient } from '@/components/providers/get-query-provider';
+import { SidebarProvider } from '@/context/sidebar';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
-import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => getQueryClient());
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
