706882047e870c6c207aee03f4fb5d3d9c59db0b
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-09-28 14:31:47 +0300

    Refact : middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+
   const authRoutes = ['/login', '/forgot-password'];
+  const publicRoutes = ['/',...authRoutes];
   const { pathname } = request.nextUrl;
 
   // Get token from cookies in the request context
   const token = request.cookies.get('token')?.value;
 
-  // If user is on auth routes and has token, redirect to dashboard
-  if (authRoutes.includes(pathname) && token) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if(!publicRoutes.includes(pathname) ) {
+
+    if(token) return NextResponse.next()
+
+      const redirectUrl = new URL('/login', request.nextUrl.origin)
+      redirectUrl.searchParams.set('callbackUrl', pathname)
+      return NextResponse.redirect(redirectUrl)
   }
 
-  // If user is on protected routes (not auth routes) and has no token, redirect to login
-  if (!authRoutes.includes(pathname) && !token) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if(authRoutes.includes(pathname) ) {
+    if(!token) return NextResponse.next()
+
+      const redirectUrl = new URL('/', request.nextUrl.origin)
+      return NextResponse.redirect(redirectUrl)
   }
 
-  // If user is on root path and has token, allow access to dashboard
-  if (pathname === '/' && token) {
-    return NextResponse.next();
-  }
+  return NextResponse.next()
 
-  // If user is on root path and has no token, redirect to login
-  if (pathname === '/' && !token) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
-  return NextResponse.next();
 }
-
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
